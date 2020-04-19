24ca1cd24b769c1a0a789eb270ed8452e0d43c20
BuildTools <unconfigured@null.spigotmc.org>
2020-04-19 12:58:33 -0500

    proj

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+
+public class PlayerMove : MonoBehaviour
+{
+    
+    CharacterController characterController;
+    
+    //public Camera mainCamera;
+    public float speed = 6.0f;
+    //public float jumpMoveSpeed = 3.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    //private Vector3 cameraDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        //mainCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        /*else {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= jumpMoveSpeed;
+        }*/
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        //cameraDirection = mainCamera.transform.forward;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
